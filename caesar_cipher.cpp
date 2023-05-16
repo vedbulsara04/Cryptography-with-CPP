ce82ebd40924227663f85f7116747118d669399e
Ved Bulsara <107607613+vedbulsara04@users.noreply.github.com>
2023-05-16 08:54:50 +0530

    Update caesar_cipher.cpp

--- caesar_cipher.cpp
+++ caesar_cipher.cpp
@@ -1,58 +1,56 @@
 #include<iostream>
+#include<string>
 #include<string.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+string caesar_cipher_encrypt(string message, int shift) {
+    string encrypted_message;
+    for(int i = 0; i < message.length(); i++) {
+        encrypted_message += char(int(message[i]) + shift);
+    }
+    cout << "Encrypted Message = " << encrypted_message << endl;
+    return encrypted_message;
+}
+
+void caesar_cipher_decrypt(string encrypted_message, int shift) {
+    string decrypted_message;
+    for(int i = 0; i < encrypted_message.length(); i++) {
+        decrypted_message += char(int(encrypted_message[i]) - shift);
+    }
+    cout << "Decrypted Message = " << decrypted_message << endl;
+}
+
 int main() {
-   cout<<"Enter the message:\n";
-   char msg[100];
-   cin.getline(msg,100);
 
-   int i, j, length,choice,key;
-   cout << "Enter key: ";
-   cin >> key;
+    string output;
 
-   length = strlen(msg);
-   cout<<"Enter your choice \n1. Encryption \n2. Decryption \n";
-   cin>>choice;
+                int s;
+                cout << "Enter the shift size: ";
+                cin >> s;
 
-      char ch;
-      for(int i = 0; msg[i] != '\0'; ++i) {
-         ch = msg[i];
-         if (ch >= 'a' && ch <= 'z'){
-            ch = ch + key;
-            if (ch > 'z') {
-            ch = ch - 'z' + 'a' - 1;
-            }
-            msg[i] = ch;
-         }
-         else if (ch >= 'A' && ch <= 'Z'){
-            ch = ch + key;
-            if (ch > 'Z'){
-               ch = ch - 'Z' + 'A' - 1;
-            }
-            msg[i] = ch;
-         }
-      }
-      printf("Encrypted message: %s", msg);
-
-
-
-      for(int i = 0; msg[i] != '\0'; ++i) {
-         ch = msg[i];
-         if(ch >= 'a' && ch <= 'z') {
-            ch = ch - key;
-            if(ch < 'a'){
-               ch = ch + 'z' - 'a' + 1;
-            }
-            msg[i] = ch;
-         }
-         else if(ch >= 'A' && ch <= 'Z') {
-            ch = ch - key;
-            if(ch < 'A') {
-               ch = ch + 'Z' - 'A' + 1;
-            }
-            msg[i] = ch;
-         }
-      }
-      cout <<"\nDecrypted message: "<< msg;
-
+                cout << "1. Encrypt \n";
+                cout << "2. Decrypt \n";
+                cout << "Enter your choice: ";
+                int choice;
+                cin >> choice;
+                if(choice == 1) {
+                    string message;
+                    cout << "Enter the message to be encrypted: ";
+                    cin >> message;
+                    output = caesar_cipher_encrypt(message, s);
+                    int n;
+                    cout << "Press (1) if you want to decrypt else (0): ";
+                    cin >> n;
+                    if(n == 1) {
+                        caesar_cipher_decrypt(output, s);
+                    }
+                }
+                else {
+                    string message;
+                    cout << "Enter the message to be decrypted: ";
+                    cin >> message;
+                    caesar_cipher_decrypt(message, s);
+                }
+    return 0;
 }
